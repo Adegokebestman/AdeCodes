4fac5c58877abd29037e9d1a305f5884da4e46c4
adegoke bestman <adegokebestman@gmail.com>
2023-08-10 12:18:05 +0100

    updated

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,17 +5,14 @@ import {visionTool} from '@sanity/vision'
 // import { netlifyWidget } from 'sanity-plugin-dashboard-widget-netlify';
 import schemas from './sanity/schemas';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
 
 export default defineConfig({
   name: 'default',
   title: 'Adecodes',
 
-  projectId,
-  apiVersion,
-  dataset,
+  projectId: '1vabhok1',
+  apiVersion: '2023-08-10',
+  dataset: 'production',
   basePath: '/admin',
 
   plugins: [

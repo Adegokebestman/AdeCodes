40e5f883fb2ae6d42074bef9a938a728a6cacbc0
adegoke bestman <adegokebestman@gmail.com>
2024-07-26 06:33:23 +0100

    chore: Update Google Analytics script in layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
+const NEXT_PUBLIC_GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
   title: 'Adegoke Bestman - Developer & Designer',
@@ -22,11 +23,30 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <script
+        strategy="afterInteractive"
+        defer
+        src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+       <script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <body className={inter.className}>
-      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+      {/* {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
             <GoogleAnalytics ga_id=
             {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-          ) : null}
+          ) : null} */}
       <Navbar />
         <div className='overflow-hidden mx-auto flex-col align-center justify-center py-6 md:px-5 px-2 xl:px-0 '>
 
